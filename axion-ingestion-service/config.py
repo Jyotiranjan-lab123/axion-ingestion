4f903e060f18e471394e116b8d346a5775e0dd16
jyoti <samaljyotiranjan24@gmail.com>
2026-09-08 13:57:16 +0530

    new code

--- axion-ingestion-service/config.py
+++ axion-ingestion-service/config.py
@@ -14,7 +14,7 @@ class Settings:
     # Example: postgresql://postgres:postgres@localhost:5432/axiondb
     DATABASE_URL: str = os.getenv(
         "DATABASE_URL",
-         "postgresql://admin:Password@12345@10.244.0.48:5432/axiondbb"
+         "postgresql://admin:Password%4012345@postgresql:5432/axiondbb"
     )
 
 settings = Settings()
